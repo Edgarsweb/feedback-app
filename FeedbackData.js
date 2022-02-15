e0a21c63714198d52e38fe6907b86c2bbf3d8692
Edgar Hovhannisyan <78177055+Edgarsweb@users.noreply.github.com>
2022-02-16 00:42:10 +0400

    Add files via upload

---
+++ FeedbackData.js
@@ -0,0 +1,27 @@
+const FeedbackData = [
+    
+    {
+        id: 1,
+        rating: 10,
+        text: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum assumenda dicta delectus facere quae.',
+    },
+
+
+    {
+        id: 2,
+        rating: 9,
+        text: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum assumenda dicta delectus facere quae.',
+    },
+
+    {
+        id: 3,
+        rating: 8,
+        text: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum assumenda dicta delectus facere quae.',
+    },
+
+    
+
+
+] 
+
+export default FeedbackData
